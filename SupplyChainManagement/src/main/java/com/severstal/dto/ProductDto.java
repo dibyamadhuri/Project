270695ff97640e31bcb48d4902df48db240a25dd
Dibya <Dibya@DESKTOP-O92K9TQ>
2021-10-04 10:06:47 +0530

    add toString and getter and setter method

--- SupplyChainManagement/src/main/java/com/severstal/dto/ProductDto.java
+++ SupplyChainManagement/src/main/java/com/severstal/dto/ProductDto.java
@@ -1,5 +1,22 @@
 package com.severstal.dto;
 
 public class ProductDto {
-
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	@Override
+	public String toString() {
+		return "ProductDto [id=" + id + ", name=" + name + "]";
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
